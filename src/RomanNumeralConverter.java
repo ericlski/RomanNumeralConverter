380abc47c74b9ab331d9e1e6f5307bf2c8a5c775
Eric Leszcynski <ericlski@vt.edu>
2018-12-14 12:51:28 -0500

    basic subtraction working

--- src/RomanNumeralConverter.java
+++ src/RomanNumeralConverter.java
@@ -20,6 +20,7 @@ public class RomanNumeralConverter {
      */
     public static int r2i(String roman) throws IllegalArgumentException {
         int result = 0;
+        char previousChar = 0;
         // Conversion code
         for (char x: roman.toCharArray()) {
             switch (x){
@@ -27,26 +28,51 @@ public class RomanNumeralConverter {
                     result += 1;
                     break;
                 case 'V':
+                    if (previousChar == 'I') {
+                        // need to remove two as one was already added to result in the previous run thru the loop
+                        result -= 2;
+                    }
                     result += 5;
                     break;
                 case 'X':
+                    if (previousChar == 'I') {
+                        // need to remove two as one was already added to result in the previous run thru the loop
+                        result -= 2;
+                    }
                     result += 10;
                     break;
                 case 'L':
+                    if (previousChar == 'X') {
+                        // need to remove twenty as ten was already added to result in the previous run thru the loop
+                        result -= 20;
+                    }
                     result += 50;
                     break;
                 case 'C':
+                    if (previousChar == 'X') {
+                        // need to remove twenty as ten was already added to result in the previous run thru the loop
+                        result -= 20;
+                    }
                     result += 100;
                     break;
                 case 'D':
+                    if (previousChar == 'C') {
+                        // need to remove 200 as 100 was already added to result in the previous run thru the loop
+                        result -= 200;
+                    }
                     result += 500;
                     break;
                 case 'M':
+                    if (previousChar == 'C') {
+                        // need to remove 200 as 100 was already added to result in the previous run thru the loop
+                        result -= 200;
+                    }
                     result += 1000;
                     break;
                 default:
                     throw new IllegalArgumentException("Undefined Roman Numeral Given");
             }
+            previousChar = x;
         }
         return result;
     }
